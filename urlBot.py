037ba1fddd6c1e994e7942e882a4be11d57ab5d5
eternnoir <eternnoir@gmail.com>
2014-07-14 16:57:05 +0800

    fix ascii problem

--- urlBot.py
+++ urlBot.py
@@ -14,16 +14,16 @@ import time
 import config
 
 
-
 class Sender(object):
-    def __init__(self, urlbot,c, url,senderId, at_time):
+
+    def __init__(self, urlbot, c, url, senderId, at_time):
         self.thread = Thread(target=self.process)
         self.url = url
-        self.urlbot=urlbot
-        self.at_time=at_time
+        self.urlbot = urlbot
+        self.at_time = at_time
         self.senderId = senderId
         self.title_length = 300
-        self.max_page_size=1048576
+        self.max_page_size = 1048576
         self.c = c
 
     def start(self):
@@ -40,23 +40,36 @@ class Sender(object):
             time.sleep(1)
         print("process %r" % self.url)
         try:
-            soup = BeautifulSoup(urllib2.urlopen(self.url).read(self.max_page_size))
+            soup = BeautifulSoup(
+                urllib2.urlopen(
+                    self.url).read(
+                    self.max_page_size))
         except urllib2.HTTPError as e:
             print("HTTPError when fetching %s : %s\n" % (e.url, e))
             return
         if not soup.title:
             return
         if len(soup.title.string) > self.title_length:
-            title=soup.title.string[0:self.title_length] + u'…'
+            title = soup.title.string[0:self.title_length] + u'…'
         else:
-            title=soup.title.string
-            self.urlbot.say(self.c,self.senderId+"'s url : [ "+ title.strip()+" ]")
+            title = soup.title.string
+            self.urlbot.say(
+                self.c,
+                self.senderId +
+                "'s url : [ " +
+                title.strip() +
+                " ]")
+
 
 class Bot(irc.bot.SingleServerIRCBot):
+
     def __init__(self, channel, nickname, server, port=6667):
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        irc.bot.SingleServerIRCBot.__init__(
+            self, [
+                (server, port)], nickname, nickname)
         self.channel = channel
-        self.url_regexp=re.compile("""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")
+        self.url_regexp = re.compile(
+            """((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -68,27 +81,26 @@ class Bot(irc.bot.SingleServerIRCBot):
         self.do_command(e, e.arguments[0])
 
     def on_pubmsg(self, c, e):
-        idName= e.source.nick
+        idName = e.source.nick
         data = e.arguments[0]
         for url in re.findall(self.url_regexp, data):
-            url=url[0]
+            url = url[0]
             if not url.startswith('http'):
-                url='http://'+url
-            print url
-            Sender(self,c,url,idName,1 ).start()
+                url = 'http://'+url
+            print url.encode('utf-8')
+            Sender(self, c, url, idName, 1).start()
 
-
-    def say(self,c,msg):
-        c.privmsg(self.channel,msg)
+    def say(self, c, msg):
+        c.privmsg(self.channel, msg)
 
 
 def main():
-    channel= config.channel
-    nickname= config.nickname
-    server= config.server
+    channel = config.channel
+    nickname = config.nickname
+    server = config.server
     port = config.port
-    bot = Bot(channel, nickname, server,port)
+    bot = Bot(channel, nickname, server, port)
     bot.start()
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
